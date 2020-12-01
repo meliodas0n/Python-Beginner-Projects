320d7582a201cc2fd2c75fce9d78926a9b09e47a
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-01 13:01:11 +0530

    added color and removed few print statements

--- number__guess.py
+++ number__guess.py
@@ -1,41 +1,30 @@
 import random
+from termcolor import colored
+#pip install termcolor
 
+print('Welcome to the Number Guess game\nHope U will have lots of FUN!!!')
 
-print("Hello there\n")
-print("Welcome to the Number Guess game\n")
-print("Hope you will have lots of FUN!!!")
-
-random_number = random.randint(0, 1001)
-win = False
-intial_turns = 1
+random_number = random.randint(0,100)
+win, intial_turns =False, 1
 
 print("Tell me in how many terms you can guess the number\n")
 turns = input()
 
 while win == False and intial_turns <= int(turns):
     intial_turns += 1
-    your_number = input("Enter a number between 0 to 1000\n")
+    your_number = input("Enter a number between 0 to 99\n")
     if your_number == random_number:
-        print("You WON!\n")
-        print("Congratulations on guessing the CORRECT NUMBER\n")
+        print(colored('You WON! Congratulations on guessing the CORRECT NUMBER\n','blue'))
         win = True
         
     else:
-        print("You guessed the WRONG NUMBER!\n")
-        print("Keep trying")
+        print('You guessed the WRONG NUMBER!  Keep trying')
         if int(your_number) > random_number:
-            print("Your number is a bit higher,try guessing a smaller number\n")
+            print(colored("Your number is a bit higher,try guessing a smaller number\n",'red'))
         else:
-            print("Your number is bit lower,try guessing a higher number\n")
+            print(colored("Your number is bit lower,try guessing a higher number\n",'red'))
 
     if intial_turns == turns:
-        print("Sorry!! Your out of turns!!\n")
-        print("Try again next time")
-
-    print("Sorry!! Your out of turns!!\n")
-    print("Try again next time\n")
-    
-    
-print("The random number is \n")
-print(random_number)
-
+        print(colored('Sorry!! Your out of turns!! Try again next time!','red'))
+print(colored('Sorry!! Your out of turns!! Try again next time!\n','red'))
+print(colored(f'The random number is {random_number}','yellow'))
